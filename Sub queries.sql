21a9d683f85e14e17702bd075e1dd54cd2ecc49a
n0n4m3n0 <164059106+n0n4m3n0@users.noreply.github.com>
2024-03-20 18:09:01 +0300

    Update Sub queries.sql

--- Sub queries.sql
+++ Sub queries.sql
@@ -1,1 +1,9 @@
+-- 1. Show products, which units_in_stock is less the minimum average quantity of products in order_details (group by product_id).
+-- Resulting table should contain product_name and units_in_stock columns.
 
+SELECT product_name, units_in_stock
+FROM products
+WHERE units_in_stock < ALL(SELECT AVG(quantity)
+FROM order_details 
+GROUP BY product_id)
+ORDER BY units_in_stock DESC
