4c1bd860b11c2f9efb472534c10b4dc4224d4c21
n0n4m3n0 <164059106+n0n4m3n0@users.noreply.github.com>
2024-03-22 13:39:04 +0300

    Update CASE logic.sql

--- SQL/CASE logic.sql
+++ SQL/CASE logic.sql
@@ -20,3 +20,18 @@ SELECT product_name, unit_price,
 		   WHEN unit_price < 50 THEN 'low price'
 	END AS price_description
 FROM products	
+
+-- 3. Find customers who have not made a single order. Print the customer name and the value 'no orders' if order_id = NULL.
+
+SELECT company_name,
+	CASE WHEN order_id IS NULL THEN 'no orders'
+	END AS description	  
+FROM customers
+LEFT JOIN orders USING(customer_id)
+WHERE order_id IS NULL
+
+-- 4. Display the full names of employees and their positions. If position = Sales Representative, display Sales Stuff instead.
+	
+SELECT first_name, last_name, 
+	COALESCE(NULLIF(title, 'Sales Representative'), 'Sales Stuff') AS title_description
+FROM employees
