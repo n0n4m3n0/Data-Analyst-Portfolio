a8d0dd6e6f18eb6b6216dc3b50bdbeacc900c5b3
n0n4m3n0 <164059106+n0n4m3n0@users.noreply.github.com>
2024-04-18 13:26:59 +0300

    Update puzzles1.py

--- Python/puzzles1.py
+++ Python/puzzles1.py
@@ -1,1 +1,11 @@
+'''
+Puzzle 
+Define a function that takes an arbitrary number of arguments.
+When called, the function should return the number of arguments it was called with.
+'''
+from typing import Any
 
+def param_count(*args: Any):
+    count_args = len(args)
+    return count_args
+print(param_count())   
